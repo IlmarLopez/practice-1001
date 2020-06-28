946f05fbbf0dd4cd3b5691f0d1421381939bef39
Ilmar López <ilmarlopezr@hotmail.com>
2020-06-28 15:18:46 -0700

    Cambio de nombre de clase a Security

--- api/config/security.php
+++ api/config/security.php
@@ -1,18 +1,19 @@
 <?php
-  class Config {
-    // get faile url
-    public static function getFileUrl($key)
-    {
-      $data = file_get_contents(__DIR__.'/../config/config.json');
-      $config = json_decode($data, true);
-      if (isset($config['file'])) {
-        // get files settings
-        $files = $config['files'];
-        // file location
-        if (isset($files[$key]))
-          return $files[$key];
-        else
-          return '';
-      }
+class Security
+{
+  // get faile url
+  public static function getFileUrl($key)
+  {
+    $data = file_get_contents(__DIR__ . '/../config/config.json');
+    $config = json_decode($data, true);
+    if (isset($config['file'])) {
+      // get files settings
+      $files = $config['files'];
+      // file location
+      if (isset($files[$key]))
+        return $files[$key];
+      else
+        return '';
     }
   }
+}
