dff108b49101e1a4e365d42961d7bcc52def5a11
Ilmar López <ilmarlopezr@hotmail.com>
2020-06-28 15:27:08 -0700

    Se creó  la clase Security.

--- api/config/config.php
+++ api/config/config.php
@@ -1,18 +1,19 @@
 <?php
-    class Config{
-        //get file url
-        public static function getFileUrl($key){
-            $data = file_get_contents(__DIR__.'/../config/config.json');
-            $config = json_decode($data, true);
-            if(isset($config['files'])){
-                //get files settings
-                $files = $config['files'];
-                //file location
-                if(isset($files[$key])){
-                    return $files[$key];
-                }else 
-                    return '';
-            }
+class Config
+{
+    //get file url
+    public static function getFileUrl($key)
+    {
+        $data = file_get_contents(__DIR__ . '/../config/config.json');
+        $config = json_decode($data, true);
+        if (isset($config['files'])) {
+            //get files settings
+            $files = $config['files'];
+            //file location
+            if (isset($files[$key])) {
+                return $files[$key];
+            } else
+                return '';
         }
     }
-?>
+}
--- api/config/security.php
+++ api/config/security.php
@@ -1,19 +1,18 @@
 <?php
 class Security
 {
-  // get faile url
-  public static function getFileUrl($key)
+  public static function generateToken($key)
   {
-    $data = file_get_contents(__DIR__ . '/../config/config.json');
-    $config = json_decode($data, true);
-    if (isset($config['file'])) {
-      // get files settings
-      $files = $config['files'];
-      // file location
-      if (isset($files[$key]))
-        return $files[$key];
-      else
-        return '';
+    $today = date_create(); // today's date
+    $token = ''; // token
+    // 0 arguments : token is date only
+    if (func_num_args() == 0)
+      $token = sha1(date_format($today, 'Ymd'));
+    // 1 argument : token us userid and date
+    if (func_num_args() === 1) {
+      $user = func_get_args(0); // user
+      $token = sha1($user . (date_format($today, 'Ymd')));
     }
+    return $token; // return token
   }
 }
