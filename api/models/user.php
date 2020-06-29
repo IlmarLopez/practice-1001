40b10348de11ef458984aaf05263fad3088ad874
Ilmar López <ilmarlopezr@hotmail.com>
2020-06-28 17:16:45 -0700

    Bug: devuelve usaurio con un array de roles

--- api/models/user.php
+++ api/models/user.php
@@ -1,8 +1,11 @@
 <?php
 //require files
 require_once('mysqlconnection.php');
-require_once('userrole.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/dashboard2020/api/config/exceptions/recordnotfoundexception.php');
+require_once('role.php');
+require_once(__DIR__ . '/../config/exceptions/accessdeniedexception.php');
+require_once(__DIR__ . '/../config/exceptions/recordnotfoundexception.php');
+require_once(__DIR__ . '/../config/config.php');
+// require_once($_SERVER['DOCUMENT_ROOT'] . '/dashboard2020/api/config/exceptions/recordnotfoundexception.php');
 class User
 {
     //attributes
@@ -38,10 +41,6 @@ class User
     {
         return $this->photo = $photo;
     }
-    public function getRoles()
-    {
-        return $this->roles;
-    }
     public function getPassword()
     {
         return $this->password;
@@ -60,7 +59,6 @@ class User
         if (func_num_args() == 0) {
             $this->id = '';
             $this->name = '';
-            $this->roles = [];
             $this->photo = '';
             $this->password = '';
         }
@@ -93,13 +91,40 @@ class User
             $connection->close(); //close conection
 
         }
-        //create an empty object
-        if (func_num_args() == 4) {
+
+        // login
+        if (func_num_args() == 2) {
+            // parameters
+            $username = $arguments[0];
+            $password = $arguments[1];
+
+            $query = 'SELECT id, name, photo FROM users where id = ? and password = sha1(?);'; // query
+            $connection = MySqlConnection::getConnection(); // get connection
+            $command = $connection->prepare($query); //prepare command (avoid injection)
+            $command->bind_param('ss', $username, $password);
+            $command->bind_result(
+                $id,
+                $name,
+                $photo,
+            ); //bind results
+            $command->execute(); //execute
+            // record found
+            if ($command->fetch()) {
+                $this->id = $id;
+                $this->name = $name;
+                $this->photo = $photo;
+            } else {
+                throw new AccessDeniedException($username);
+            }
+            mysqli_stmt_close($command); //close command
+            $connection->close(); //close conection
+        }
+
+        //create object with values from the arguments
+        if (func_num_args() == 3) {
             $this->id = $arguments[0];
             $this->name = $arguments[1];
             $this->photo = $arguments[2];
-            $this->password = $arguments[3];
-            $this->roles = $this->getAllRoles();
         }
         //create object with data  from the arguments
     }
@@ -113,15 +138,15 @@ class User
             'id' => $this->id,
             'name' => $this->name,
             'roles' => $this->roles,
-            'photo' => $this->photo,
-            'password' => $this->password
+            'photo' => Config::getFileUrl('userPhotos') . $this->photo,
+            'roles' => json_decode($this->getRolesToJson()),
         ));
     }
     public function toJsonFull()
     {
         //get roles
         $list = array(); //empty list
-        foreach ($this->getAllRoles() as $item) {
+        foreach ($this->getRoles() as $item) {
             //add item to list
             array_push($list, json_decode($item->toJson()));
         }
@@ -130,35 +155,36 @@ class User
             'id' => $this->id,
             'name' => $this->name,
             'photo' => $this->photo,
-            'password' => $this->password,
             'roles' => $list
         ));
     }
 
 
     //return a list of all the roles
-    public function getAllRoles()
+    public function getRoles()
     {
         $list = array(); // empty list
-        $query = 'select name from roles as r inner join usersroles as ur on r.id = ur.idRole where idUser = ?;';
+        $query = 'select r.id, r.name from roles as r join usersroles as ur on r.id = ur.idRole where idUser = ?;';
         $connection = MySqlConnection::getConnection(); //connection
         $command = $connection->prepare($query); //command
         $command->bind_param('s', $this->id); //bind parameters
+        $command->bind_result($id, $name); //bind results
         $command->execute();
-        $command->bind_result($name); //bind results
         //read rows
         while ($command->fetch()) {
             //add readings to list
-            array_push($list, new UserRole($name));
+            array_push($list, new Role($id, $name));
         }
+        mysqli_stmt_close($command); // close statement
+        $connection->close(); // close connection
         return $list; //return list
     }
 
     //return a json of all the roles
-    public static function getAllRolesToJson()
+    public function getRolesToJson()
     {
         $list = array(); //empty list
-        foreach ($this->getAllRoles() as $item) {
+        foreach ($this->getRoles() as $item) {
             //add item to list
             array_push($list, json_decode($item->toJson()));
         }
@@ -172,19 +198,18 @@ class User
     {
         $list = array(); //empty list
         //query to users
-        $query = 'SELECT id, name, photo, password FROM users;';
+        $query = 'SELECT id, name, photo FROM users order by name;';
         $connection = MySqlConnection::getConnection(); // get connection
         $command = $connection->prepare($query); //prepare command (avoid injection)
         $command->bind_result(
             $id,
             $name,
             $photo,
-            $password,
         ); //bind results
         $command->execute(); //execute
         //read results
         while ($command->fetch()) {
-            array_push($list, new User($id, $name, $photo, $password));
+            array_push($list, new User($id, $name, $photo));
         }
         mysqli_stmt_close($command); //close command
         $connection->close(); //close conection
--- api/userscontroller.php
+++ api/userscontroller.php
@@ -1,38 +1,44 @@
-<?php
-//require files
-require_once('models/user.php');
-//require_once('config/exceptions/recordnotfoundexception.php');
+    <?php
+    //require files
+    require_once('models/user.php');
+    //require_once('config/exceptions/recordnotfoundexception.php');
 
-//get
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if ($parameters == '') {
-        //get all
-        if ($action == '') {
-            echo json_encode(array(
-                'status' => 0,
-                'users' => json_decode(User::getAllToJson())
-            ));
-        }
-        // login
-        if ($action == 'login') {
-            // read headers
-            $headers = getallheaders();
-            // verify headers
-            if (isset($headers['username']) && isset($headers['password'])) {
-            } else
-                echo json_encode(array('status' => 501, 'errorMessage' => 'Missing security headers'));
-        }
-    } else {
-        //get one
-        try {
-            $d = new User($parameters);
-            //display object
-            echo json_encode(array(
-                'status' => 0,
-                'user' => json_decode($d->toJsonFull())
-            ));
-        } catch (RecordNotFoundException $ex) {
-            echo json_encode(array('status' => 1, 'errorMessage' => $ex->getMessage()));
+    //get
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        if ($parameters == '') {
+            //get all
+            if ($action == '') {
+                echo json_encode(array(
+                    'status' => 0,
+                    'users' => json_decode(User::getAllToJson())
+                ));
+            }
+            // login
+            if ($action == 'login') {
+                // read headers
+                $headers = getallheaders();
+                // verify headers
+                if (isset($headers['username']) && isset($headers['password'])) {
+                    try {
+                        $u = new User($headers['username'], $headers['password']);
+                        echo json_encode(array('status' => 0, 'user' => json_decode($u->toJson())));
+                    } catch (AccessDeniedException $ex) {
+                        echo json_encode(array('status' => 500, 'errorMessage' => $ex->getMessage()));
+                    }
+                } else
+                    echo json_encode(array('status' => 501, 'errorMessage' => 'Missing security headers'));
+            }
+        } else {
+            //get one
+            try {
+                $d = new User($parameters);
+                //display object
+                echo json_encode(array(
+                    'status' => 0,
+                    'user' => json_decode($d->toJsonFull())
+                ));
+            } catch (RecordNotFoundException $ex) {
+                echo json_encode(array('status' => 1, 'errorMessage' => $ex->getMessage()));
+            }
         }
     }
-}
